a0cedc5f8b032efa81566a01dec6b7e27eb221cf
snawone1 <s.n.a.w.2007@gmail.com>
2023-08-21 23:12:04 -0300

    modulo 05 Completo

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -114,7 +114,16 @@ function multiplicarArgumentos() {
    // Si no se pasan argumentos retorna 0. Si se pasa un argumento, simplemente retórnalo.
    // [PISTA]: "arguments" es un arreglo.
    // Tu código:
+   var resuMulti = 1;
+   if(arguments.length  == 0){
+      return 0;
+   }
    
+   for(let i = 0; i < arguments.length; i++){
+      resuMulti *= arguments[i];
+       
+   }
+   return resuMulti;
 }
 
 function cuentoElementos(array) {
